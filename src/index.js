802d4382a05239b0d890e74f5dea6533d69f5d87
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2017-05-16 14:49:43 -0500

    remove map

---
+++ src/index.js
@@ -0,0 +1,4 @@
+export { setAspectRatio } from './setAspectRatio';
+export { createGradient } from './createGradient';
+export { em } from './em';
+export { visuallyHideElement } from './visuallyHideElement';
